ea0c3837d9f709b2151c82706606859d800be484
ThanhThien66 <kudomail1412@gmail.com>
2023-08-23 20:21:15 +0700

    1332

--- lab3.2/index.js
+++ lab3.2/index.js
@@ -6,5 +6,5 @@ app.get('/someUri', function (req, res, next) {})
 app.route('/myPath')
  .get(function (req, res, next) {})
  .post(function (req, res, next) {})
- .put(function (req, res, next) {}
+ .put(function (req, res, next) {})
 app.get('/myPath', myFunction, function (req, res, next) {});
